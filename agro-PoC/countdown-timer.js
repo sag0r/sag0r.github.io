545cc058c27758c628d956f9b4bb916bfaaa6fb6
Sagor <ifunctional713@gmail.com>
2020-10-25 11:51:57 +0600

    new

---
+++ agro-PoC/countdown-timer.js
@@ -0,0 +1,39 @@
+function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds
+    };
+}
+
+function initializeClock(selector, endtime) {
+    const clock = document.querySelector(selector);
+    const daysSpan = clock.querySelector('.days .clock-text');
+    const hoursSpan = clock.querySelector('.hours .clock-text');
+    const minutesSpan = clock.querySelector('.minutes .clock-text');
+    const secondsSpan = clock.querySelector('.seconds .clock-text');
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+}
--- agro-PoC/script.js
+++ agro-PoC/script.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
 
+    // Show tabs on hover instead of click
     $('.shop-dropdown .nav-link')
         .hoverIntent(function () {
             $(this).tab('show');
         });
 
+    // Banner typing-text effect
     const typed = new Typed('#typed', {
         stringsElement: '#typed-strings',
         typeSpeed: 50,
         loop: true,
         loopCount: Infinity,
     });
+
+    // Countdown clock
+    const deadline = new Date(Date.parse(new Date()) + 2 * 24 * 60 * 60 * 1000);
+    initializeClock('.countdown-clock', deadline);
 
 
 
